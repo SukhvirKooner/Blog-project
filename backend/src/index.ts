98dc8b2b1d109e2b79fd67091f0c8dc414530074
SukhvirKooner <sukhvirkooner05@gmail.com>
2024-10-30 12:20:24 +0530

    signup backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,32 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
+const app = new Hono<{
+	Bindings: {         // in hono u have to pass this thing to specify the type of database url
+		DATABASE_URL: string
+	}
+}>();
 
+// in serverless backend , we should avoid global variables bcz 
+ // depending on the runtime , they just start the specific function
+// u might loose access to global contents
+app.post('/api/v1/user/signup', async(c)=>{
+  const prisma = new PrismaClient({     
+    datasourceUrl: c.env.DATABASE_URL, 
+}).$extends(withAccelerate()) 
 
+const body = await c.req.json();
 
-app.post('/api/v1/user/signup', (c)=>{
-  const prisma = new PrismaClient({
-    datasourceUrl: env.DATABASE_URL,
-}).$extends(withAccelerate())
+  await prisma.user.create({
+    data:{
+      email: body.email,
+      name: body.name,
+      password: body.password
+    }
+  })
+  
   return c.text("POST signup")
 })
 
